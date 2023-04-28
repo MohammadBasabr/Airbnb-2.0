5ec17ac7ff5cffa4f27d35b2651fbb45dbbf6287
Mohammad Baasabr <m.basabr75@gmail.com>
2023-04-29 00:04:24 +0330

    fix: some env variables

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
     domains: ["links.papareact.com"]
   },
   logLevel: "trace",
+  env: {
+    HOST_URL: "https://airbnb-2-0-n2ow0b8ub-mbasabr75-gmailcom.vercel.app"
+  }
 }
 
 module.exports = nextConfig
